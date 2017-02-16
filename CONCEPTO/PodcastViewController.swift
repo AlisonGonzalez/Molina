f7edd1a7ebce5662d630345f05c8875d03f4e7f8
irvin <irvinumundo@gmail.com>
2017-02-16 13:25:37 -0600

    Commit 4

---
+++ CONCEPTO/PodcastViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PodcastViewController.swift
+//  CONCEPTO
+//
+//  Created by Irvin Mundo on 16/02/17.
+//  Copyright © 2017 Alfredo. All rights reserved.
+//
+
+import UIKit
+
+class PodcastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var buscaPodcast: UISearchBar!
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
