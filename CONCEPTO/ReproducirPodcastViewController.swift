f2146c29242d73781068b9e54ad3d9048d0fe20d
Alfredo <alfredo@Alfredos-iMac.local>
2017-03-30 01:42:11 -0600

    Intenté JSON pero no funciona

--- CONCEPTO/ReproducirPodcastViewController.swift
+++ CONCEPTO/ReproducirPodcastViewController.swift
@@ -11,120 +11,50 @@ import AVFoundation
 
 class ReproducirPodcastViewController: UIViewController {
     
-    fileprivate var reproductor: AVAudioPlayer!
+    
     
     @IBOutlet weak var name: UILabel!
 
+    var nombre:String = ""
     var link:String = ""
     
-    var nombre:String = ""
+    var player = AVAudioPlayer()
     
-    let sharedInstance = RadioPlayer()
+    var sonido:NSURL? = nil
     
-    var player = AVPlayer(url: NSURL(string: link) as! URL)
+    var error = NSError()
     
-    
+    var reproduciendo = false
     override func viewWillAppear(_ animated: Bool) {
-        /*
-        name.text = link
+       name.text = nombre
+        sonido = NSURL(string: link)
         
-        // Do any additional setup after loading the view.
         
-        var createError: NSError?
-        let soundURL = NSURL(string: link)
         
-        do{
-            let sonido = try AVAudioPlayer(contentsOf: soundURL as! URL)
-            reproductor = sonido
-            
-        }catch let error as NSError{
-            createError = error
-            reproductor = nil
-        }
-        if reproductor == nil{
-            if let error = createError{
-                print("Error en audio \(error.localizedDescription)")
-            }
-        }*/
+        
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        player = AVPlayer(url: NSURL(string: link) as! URL)
+       
         
     }
 
     @IBAction func reproducir(_ sender: Any) {
-        play()
-    }
-    
-    @IBAction func pausa(_ sender: Any) {
-        pause()
-    }
-    
-    @IBAction func detener(_ sender: Any) {
-        stop()
-    }
-    /*func play() {
-        if !reproductor.isPlaying{
-            reproductor.play()
+        do{
+            player = try AVAudioPlayer(contentsOf:sonido as! URL)
+            player.prepareToPlay()
+            player.play()
+        }catch{
+            print(error.localizedDescription)
         }
     }
     
-    func pause() {
-        if reproductor.isPlaying{
-            reproductor.pause()
-        }
-    }
-    
-    func stop() {
-        reproductor.stop()
-    }*/
-    
-    
-    private var isPlaying = false
-    
-    func play() {
-        player.play()
-        isPlaying = true
-    }
-    
-    func pause() {
-        player.pause()
-        isPlaying = false
-    }
-    
-    func stop(){
-        
-    }
-    
-    func toggle() {
-        if isPlaying == true {
-            pause()
-        } else {
-            play()
-        }
-    }
-    
-    func currentlyPlaying() -> Bool {
-        return isPlaying
-    }
-    
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
