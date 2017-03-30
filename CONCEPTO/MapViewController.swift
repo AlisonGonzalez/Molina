aed54c154e3d58176d89f8ebcb1a5bed1ff1c739
cdt307 <cdt307@imac1-138.local>
2017-03-30 13:56:09 -0600

    mapas listos

--- CONCEPTO/MapViewController.swift
+++ CONCEPTO/MapViewController.swift
@@ -16,12 +16,15 @@ class MapViewController: UIViewController,CLLocationManagerDelegate {
     @IBOutlet weak var mapa: MKMapView!
     
     //mapa eventos 1
+    @IBOutlet weak var mapaEventos1: MKMapView!
     
     
     //mapa eventos 2
+    @IBOutlet weak var mapaEventos2: MKMapView!
     
     
     let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -43,22 +46,64 @@ class MapViewController: UIViewController,CLLocationManagerDelegate {
         pin.subtitle = "Tlalpan"
         mapa.addAnnotation(pin)
         
-        
         mapa.showsCompass=true
         mapa.showsScale=true
         mapa.showsTraffic=true
         mapa.isZoomEnabled=true
         
+        
+        //mapa eventos 1
+        mapaEventos1.mapType=MKMapType.standard
+        //coordenadas auditorio nacional
+        let cl1=CLLocationCoordinate2DMake(19.424722, -99.194917)
+        mapaEventos1.region=MKCoordinateRegionMakeWithDistance(cl1, 10, 10)
+        var punto1 = CLLocationCoordinate2D()
+        punto1.latitude = 19.283996
+        punto1.longitude = -99.136006
+        let pin1 = MKPointAnnotation()
+        pin1.coordinate = punto1
+        pin1.title = "Björk en CDMX"
+        pin1.subtitle = "Auditorio Nacional"
+        mapaEventos1.addAnnotation(pin1)
+        
+        mapaEventos1.showsCompass=true
+        mapaEventos1.showsScale=true
+        mapaEventos1.showsTraffic=true
+        mapaEventos1.isZoomEnabled=true
+        
+        
+        //mapa eventos 2
+        mapaEventos1.mapType=MKMapType.standard
+        //coordendas foro sol
+        let cl2=CLLocationCoordinate2DMake(19.404889, -99.095944)
+        mapaEventos1.region=MKCoordinateRegionMakeWithDistance(cl2, 10, 10)
+        var punto2 = CLLocationCoordinate2D()
+        punto2.latitude = 19.283996
+        punto2.longitude = -99.136006
+        let pin2 = MKPointAnnotation()
+        pin2.coordinate = punto2
+        pin2.title = "Depeche Mode en México"
+        pin2.subtitle = "Foro Sol"
+        mapaEventos2.addAnnotation(pin2)
+        
+        mapaEventos2.showsCompass=true
+        mapaEventos2.showsScale=true
+        mapaEventos2.showsTraffic=true
+        mapaEventos2.isZoomEnabled=true
+        
     }
     
-    func locationManager(_ manager: CLLocationManager,
-                         didChangeAuthorization status: CLAuthorizationStatus) {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse{
             locationManager.startUpdatingLocation()
             mapa.showsUserLocation = true
+            mapaEventos1.showsUserLocation = true
+            mapaEventos2.showsUserLocation = true
         } else {
             locationManager.stopUpdatingLocation()
             mapa.showsUserLocation = false
+            mapaEventos1.showsUserLocation = false
+            mapaEventos2.showsUserLocation = false
         }
     }
     
